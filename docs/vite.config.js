5546529fa3e07f2007eb07fc45b25773e2e1d5de
jfcuadra <jfcocuadra@gmail.com>
2025-04-29 13:09:25 -0400

    correct vite config5

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/webAlicura/',
+  base: '/webAlicura/', // Usa el nombre del repo tal cual
   plugins: [react()],
-})
+});
