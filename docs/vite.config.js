c5193691015c5da7b51366573d514c912ec7d254
jfcuadra <jfcocuadra@gmail.com>
2025-04-29 13:21:41 -0400

    plantilla

--- docs/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/webAlicura/', // Usa el nombre del repo tal cual
-  plugins: [react()],
-});
